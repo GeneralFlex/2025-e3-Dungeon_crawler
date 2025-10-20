bc4ce7814c41435c7efbbd6ef6176b059fb805d9
GeneralFlex <kloudavincent@gmail.com>
2025-10-20 22:36:40 +0200

    Player movement
    Basic player movement implementation

---
+++ Dungeon_crawler/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float walkSpeed = 4f;
+    [SerializeField] private float runSpeed = 7f;
+    [SerializeField] private float diagonalSpeed = 1.4f;
+    [SerializeField] private float smoothTime = 0.1f;
+    private float speed;
+    [HideInInspector]
+    public float speedDebuff;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    private Vector2 velocity = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // input keys stored in movement vector
+        movement = new Vector2();
+        if (Input.GetKey(KeyCode.W)) movement.y += 1;
+        if (Input.GetKey(KeyCode.S)) movement.y -= 1;
+        if (Input.GetKey(KeyCode.A)) movement.x -= 1;
+        if (Input.GetKey(KeyCode.D)) movement.x += 1;
+
+        // make the vector normalized (only direction, value=1) to prevent faster diagonal movement
+        if (movement.magnitude > 1)
+        {
+            movement = movement.normalized * diagonalSpeed;
+        }
+    }
+
+    // fix update for physics calculations :tuzar:
+    void FixedUpdate()
+    {
+        // if sprinting, use run speed, else use walk speed, apply speed debuff
+        speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed / (1 + speedDebuff) : walkSpeed / (1 + speedDebuff);
+
+        // smooth movement
+        rb.velocity = Vector2.SmoothDamp(rb.velocity, movement * speed, ref velocity, smoothTime);
+    }
+
+
+}
